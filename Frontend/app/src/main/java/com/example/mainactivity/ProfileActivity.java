c7bc30d48557bf956e81f6347f1591dd1734aa8d
Kat Fagg <katjfagg@gmail.com>
2022-10-27 10:43:01 +0100

    Login Functionality

---
+++ Frontend/app/src/main/java/com/example/mainactivity/ProfileActivity.java
@@ -0,0 +1,45 @@
+package com.example.mainactivity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.auth.User;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    private Button logout;
+    private FirebaseUser user;
+    private DatabaseReference reference;
+    private String userID;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        logout = (Button) findViewById(R.id.signOut);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(ProfileActivity.this,MainActivity.class));
+            }
+        });
+
+    }
+}
---
+++ Frontend/app/src/main/java/com/example/mainactivity/User.java
@@ -0,0 +1,16 @@
+package com.example.mainactivity;
+
+public class User {
+
+    public String fullName, email;
+
+    public User() {
+
+    }
+
+    public User(String fullName, String email){
+        this.fullName = fullName;
+        this.email = email;
+    }
+
+}
